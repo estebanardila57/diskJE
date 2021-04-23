c334f5acc84a989bbd5ec1d968d30d2643556337
ear1409 <43124834+estebanardila57@users.noreply.github.com>
2021-04-23 15:52:04 -0500

    Crud CDs

---
+++ API/CRUDTablaCDS/DiskViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proyectodisctienda.Views
+{
+    public class DiskViewModel
+    {
+        public int Id { get; set; }
+        public string Genero { get; set; }
+        public string Descripcioncd { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
